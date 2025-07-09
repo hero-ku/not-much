0aecd37fbb46d34a01c4fed668a8a8828405fe30
Andrew Lynch <a_t_lynch@protonmail.ch>
2025-07-09 10:34:14 -0500

    Add dark/light mode button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-console.log('👋🏻 hello from Hugo and imgios!');
+let themeSwitcher;
+
+document.addEventListener("DOMContentLoaded", () => {
+  themeSwitcher = document.getElementById("theme-switcher");
+  if (themeSwitcher !== undefined) {
+    if (localStorage.getItem("mode") === "true") {
+      themeSwitcher.checked = true;
+    }
+
+    themeSwitcher.addEventListener("click", () => {
+      localStorage.setItem("mode", themeSwitcher.checked);
+    });  
+  }
+});
---
+++ static/css/local function thisIsEpic().lua
@@ -0,0 +1,9 @@
+local function isEven(x)
+  return x % 2 == 0
+end
+
+isEven(1)
+print("")
+
+local myTable = { a = 2 }
+print(myTable.a)
